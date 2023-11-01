86ec3d15483481798352e2477b41d6ba8a9877a4
Denis Trischenko <deniszerg1@gmail.com>
2023-11-01 19:13:46 +0100

    add some code

---
+++ src/Main01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main01 {
+  Scanner scanner = new Scanner(System.in);
+
+  public static int readInput() {
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.println("enter num from 1 to 10");
+      int n = scanner.nextInt();
+      if (1 <= n && n <= 10) {
+        return n;
+      }
+    }
+
+  }
+
+  public static void main(String[] args) {
+    int n = readInput();
+    System.out.println("vi vveli chislo" + n);
+
+  }
+}
+
